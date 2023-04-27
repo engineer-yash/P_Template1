a7df54868aa328a90c62555c457d64251f4c7565
engineer-yash <gohelyash94@gmail.com>
2023-04-27 23:06:48 +0530

    pushed

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Yash Gohel";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,10 +71,10 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend development", "web designing", "web development"],
     loop: true,
     typeSpeed: 50,
-    backSpeed: 25,
+    backSpeed: 35,
     backDelay: 500,
 });
 // <!-- typed js effect ends -->
