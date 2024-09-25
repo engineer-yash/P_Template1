9ed027634f0afa31f03ad511efbd7c686160291b
engineer-yash <gohelyash94@gmail.com>
2024-09-25 19:40:14 +0530

    Resume - Bio - Pic

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "web designing", "web development"],
+    strings: [".Net Core", "ASP .Net MVC", "Web Development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 35,
